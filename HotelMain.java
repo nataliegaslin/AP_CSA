0d478e453e5852c4a35a9193a76e71a2b14e31f7
Engineering <Engineering@308-03.local>
2022-09-13 09:08:32 -0400

    final edits

--- HotelMain.java
+++ HotelMain.java
@@ -2,9 +2,9 @@ public class HotelMain {
   public static void main (String[] args){
     Person cecilia = new Person (5.1, 13, "Cecilia", "blonde");
     Person lux = new Person (5.6, 14, "Lux", "blonde");
-    Person therese = new Person (5.2, 15, "Therese", "blonde");
-    Person bonnie = new Person (5.7, 16, "Bonnie", "blonde");
-    Person mary = new Person (5.8, 17, "Mary", "blonde");
+    Person therese = new Person (5.2, 17, "Therese", "blonde");
+    Person bonnie = new Person (5.7, 15, "Bonnie", "blonde");
+    Person mary = new Person (5.8, 16, "Mary", "blonde");
     Person mrsLisbon = new Person (5.0, 67, "Mrs. Lisbon", "grey");
     HotelRoom room305 = new HotelRoom(305);
     ElderlySuite room306 = new ElderlySuite(306);
@@ -23,6 +23,5 @@ public class HotelMain {
     room307.printGuestInfo();
     room308.checkIn(mary);
     room308.printGuestInfo();
-
   }
 }
