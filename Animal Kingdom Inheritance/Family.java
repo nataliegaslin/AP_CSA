9aa6ca66bff5d283dea6162fec9c0ea7c440b441
nataliegaslin <112636317+nataliegaslin@users.noreply.github.com>
2022-09-20 20:49:48 -0400

    Update Family.java

--- Animal Kingdom Inheritance/Family.java
+++ Animal Kingdom Inheritance/Family.java
@@ -5,7 +5,6 @@ public class Family extends AnimalClass {
     public Family(String famNameP, String classNameP, String subphylaNameP, String phylaNameP){
         super(classNameP, subphylaNameP, phylaNameP);
         familyName = famNameP;
-    }
 
     if(familyName == "shark"){
         familyTrait = "has 7 pairs of lateral gill slits";
@@ -13,9 +12,10 @@ public class Family extends AnimalClass {
     else {
         System.out.println("Please enter a valid family name!");
     }
-
+    }
     public void printTrait(){
         System.out.println("Family: " + familyName);
         System.out.println("Trait: " + familyTrait);
         super.printTrait();
+    }
 }
