fdfe214dc3bd0d425fe57af0b2906b9382f0f91d
Engineering <Engineering@308-03.cls-edu.charlottelatin.org>
2022-10-26 09:49:54 -0400

    AP classroom

--- APClassroom/Frog.java
+++ APClassroom/Frog.java
@@ -2,36 +2,66 @@ import java.lang.Math;
 public class Frog {
     private static int goalDistance;
     private static int maxHops;
+    public static int saveDistance;
 
     public Frog (int dist, int numHops){
         goalDistance = dist;
         maxHops = numHops;
+        saveDistance = goalDistance;
     }
 
     private static int hopDistance(){
-        int rand = (int)(Math.random());
+        int rand = (int)((Math.random() * goalDistance) + 1);
         return rand;
 
     }
 
     public static boolean simulate(){
-        if (hopDistance() + hopDistance() + hopDistance() + hopDistance() + hopDistance() >= goalDistance){
-            return true;
-        }
-        else{
-            return false;
-        }
+        goalDistance = saveDistance;
+       for(int i = 0; i < maxHops; i++){
+        int distance = hopDistance();
+        System.out.println(distance);
+        goalDistance -= distance;
+
+       }
+
+       if(goalDistance < 1){
+        return true;
+       }
+
+       else{
+        return false;
+       }
 
 
     }
 
-    public double runSimulations(int num){
-        return 3.4;
+    public static double runSimulations(int num){
+        double numTrue = 0;
+        double numFalse = 0;
+        if (num > 0){
+            for(int i = 0; i <=num; i++){
+                simulate();
+                if(simulate() == true){
+                    numTrue++;
+                }
+                else{
+                    numFalse++;
+                }
+            }
+            double prop = numTrue/(numTrue + numFalse);
+            return prop;
+        }
+        else {
+            return 0.0;
+        }
 
     }
 
     public static void main(String[] args){
-        Frog bob = new Frog(10,5);
+        Frog bob = new Frog(15,5);
         System.out.println(bob.simulate());
+        System.out.println(runSimulations(10));
+
     }
 }
--- APClassroom/GameSpinner.java
+++ APClassroom/GameSpinner.java
@@ -1,29 +1,43 @@
 import java.util.Random;
-public class GameSpinner{
+import java.util.ArrayList;
+
+
+public class GameSpinner {
+    public ArrayList<Integer> myArray = new ArrayList<Integer>();
     public int sectors;
-    public int spins = 0;
     public GameSpinner(int numSectors){
         sectors = numSectors;
     }
+
     //generates a random number
     public int spin(){
         Random random = new Random();
         int number = random.nextInt(sectors);
+    
+        int l = 0;
+        while(l < 1){
+            if(myArray.isEmpty()){
+                myArray.add(number);
+            }
+            else if (myArray.get(l) == number){
+                myArray.add(number);
+            }
+            else {
+                myArray.clear();
+                myArray.add(number);
+            }
+            l++;
+        }
         return number;
 
     }
     public int currentRun(){
-        int []spinList = new int[100];
-        for(int i = 0; i<spinList.count(); i++){
-            spinList[i] = spin();
-
+        int nums = 0;
+        for(int h = 0; h < myArray.size(); h++){
+            if(myArray.get(h) != null){
+                nums++;
+            }
         }
-        System.out.println(spinList);
-
-        
-    }
-    public static void main(String[] args){
-        GameSpinner g = new GameSpinner(10);
-        System.out.println(g.spin());
-    }
+        return nums;
+       }      
 }
