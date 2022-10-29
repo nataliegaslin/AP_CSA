45a14639da59bbc810e6c98166f6616f75278362
nataliegaslin <112636317+nataliegaslin@users.noreply.github.com>
2022-10-28 20:48:34 -0400

    Update Palindrome.java

--- APClassroom/Palindrome.java
+++ APClassroom/Palindrome.java
@@ -1,5 +1,14 @@
 public class Palindrome {
+    public static String removeSpaces(String str){
+        for(int b = 0; b <str.length(); b++){
+            if(!str.substring(b, b+1).equals(" ")){
+                result += str.substring(i, i+1);
+            }
+        }
+        return result;
+    }
     public static String reverseString(String str){
+        removeSpaces(str);
         String result = "";
         for(int h = str.length()-1; h > -1; h-=1){
             result += str.substring(h, h+1);
@@ -8,6 +17,7 @@ public class Palindrome {
 
     }
     public static void palindromeChecker(String str){
+        removeSpaces(str);
         if (reverseString(str).equals(str)){
             System.out.println(str + " is a palindrome");
         }
