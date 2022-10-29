74c1c6509fd144ca8d828a15b109731737854bce
nataliegaslin <112636317+nataliegaslin@users.noreply.github.com>
2022-10-28 20:38:27 -0400

    Update Palindrome.java

--- APClassroom/Palindrome.java
+++ APClassroom/Palindrome.java
@@ -1,12 +1,9 @@
 public class Palindrome {
     public static String reverseString(String str){
-        String []reverse = new String(str.length());
-        for(int h = 0; h < str.length(); h-=1){
-            reverse[h] = str.substring(str.length() - h);
+        for(int h = str.length()-1; h > -1; h-=1){
+            String result = "";
+            result += str.substring(h, h+1)
         }
-        for(int r = 0; r < reverse.length; r++){
-            
-        }
-        return n;
+
     }
 }
