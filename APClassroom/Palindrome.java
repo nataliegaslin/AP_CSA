a27e7db8fbe4ba7e82973f2f5e07a307d45a310f
nataliegaslin <112636317+nataliegaslin@users.noreply.github.com>
2022-10-28 20:42:29 -0400

    Update Palindrome.java

--- APClassroom/Palindrome.java
+++ APClassroom/Palindrome.java
@@ -2,9 +2,17 @@ public class Palindrome {
     public static String reverseString(String str){
         String result = "";
         for(int h = str.length()-1; h > -1; h-=1){
-            result += str.substring(h, h+1)
+            result += str.substring(h, h+1);
         }
         return result;
 
+    }
+    public static void palindromeChecker(String str){
+        if (str.reverseString.equals(str)){
+            System.out.println(str + " is a palindrome");
+        }
+        else{
+            System.out.println(str + " is not a palindrome");
+        }
     }
 }
