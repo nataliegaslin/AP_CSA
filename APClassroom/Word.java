3d86212f58a19b2b30221f0e119524b35cde6369
nataliegaslin <112636317+nataliegaslin@users.noreply.github.com>
2022-10-28 20:08:39 -0400

    Update Word.java

--- APClassroom/Word.java
+++ APClassroom/Word.java
@@ -1,12 +1,10 @@
-import java.lang.*;
 public class Word {
     public String scrambleWord(String word){
         int i = 0;
         while(i < word.length() -1){
             if(word.substring(i, i+1).compareTo("A") == 0){
                 if(word.substring(i+1, i+2).compareTo("A") == 0){
-                    return word;
-                    i++
+                    i++;
                 }
                 else if(word.substring(i+1, i+2).compareTo("A") != 0){
                     word = word.substring(i+1, i+2) + word.substring(i, i+1) + word.substring(i+2);
@@ -17,4 +15,5 @@ public class Word {
                 i++;
             }
         }
+        return word;
 }
