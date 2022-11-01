bc4e452844090573dad12e398f19b2e9172ce6b7
nataliegaslin <112636317+nataliegaslin@users.noreply.github.com>
2022-11-01 19:50:38 -0400

    Update HiddenWord.java

--- APClassroom/HiddenWord.java
+++ APClassroom/HiddenWord.java
@@ -1,19 +1,29 @@
 public class HiddenWord {
     String secretWord;
-    HiddenWord(String word) {
+    
+    public HiddenWord(String word) {
         secretWord = word;
     }
     
     public String getHint(String guess){
         String returnWord = "";   
-        for(int i = 0; i < guess.length() -1; i++){      
+        for(int i = 0; i <= guess.length() -1; i++){      
             if(guess.substring(i, i+1).equals(secretWord.substring(i, i+1))){
+                returnWord = returnWord + guess.substring(i, i+1);
 
             }
                   
             
             else if(guess.substring(i, i+1).compareTo(secretWord.substring(i, i+1)) != 0){
-                if(guess.substring(i, i+1).indexOf(secretWord) <= secretWord.length)
-                    
+                if(secretWord.indexOf(guess.substring(i, i+1)) >= 0) {
+                       returnWord = returnWord + "+";
+                }
+                else{
+                    returnWord = returnWord + "*";
+                }
+          
             }
-        }        
+            return returnWord;
+        }
+        } 
+}
