e0607dbb54ab70397e969d54a1a7e804bc13d327
Engineering <Engineering@308-03.cls-edu.charlottelatin.org>
2022-11-15 09:54:57 -0500

    test

--- Minesweeper/Tile.java
+++ Minesweeper/Tile.java
@@ -1,11 +1,15 @@
 public class Tile{
     private boolean isMine = false;
     public Tile(){
-
+        //no inputs
     }
 
     public String mine(){
         isMine = true;
         return "⎈";
+
+    }
+    public String flag(){
+        
     }
 }
