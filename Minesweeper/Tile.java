e4eda27821b5ade2459b7c5f82a01ea7642439f4
nataliegaslin <112636317+nataliegaslin@users.noreply.github.com>
2022-11-28 09:16:48 -0500

    Update Tile.java

--- Minesweeper/Tile.java
+++ Minesweeper/Tile.java
@@ -4,65 +4,68 @@ public class Tile{
     private boolean isItFlipped;
     private int minesNear;
 
-    public Tile(){
+    public Tile(){ //sets up initial tile conditions
         isItAMine = false;
         isItFlagged = false;
         isItFlipped = false;
         minesNear = 0;
-   
-
     }
-    public void flip(){
+    public void flip(){ //makes the boolean isItFlipped true
        isItFlipped = true;
            
     } 
-    public void flag(){
+    public void flag(){ //makes the boolean isItFlagged true
         isItFlagged = true;
     }
+    public void unflag(){ //makes the boolean isItFlagged false
+        isItFlagged = false;
+    }
     
-    public void markMine(){
+    public void markMine(){ //makes the boolean isItAMine true
         isItAMine = true;
     }
           
-    public boolean isMine(){
+    public boolean isMine(){ //returns the value of isItAMine
         return isItAMine;
 
     }
+    public boolean returnFlagged(){ //returns the value of isItFlagged
+        return isItFlagged;
+    }
     
+    public boolean returnFlipped(){ //returns the value of isItFlipped
+        return isItFlipped;
+    }
     
-    public void setMinesNear(mineNums){
+    public void setMinesNear(int mineNums){  //changes minesNear to whatever the input is
         minesNear = mineNums;
     }
     
-    public int getMinesNear(){
+    public int getMinesNear(){ //returns minesNear
         return minesNear;
     }
     
-    public String toString(){
+    public String toString(){ //converts all of the Tile values (mine, flag, number of mines near, etc) into strings
         if(isItFlipped != true){
             if(isItFlagged != true){
-                return "[🌫]";
+                return "[ ]";
             }
             else{
-                return "[⛄]";
+                return "[⚐]";
             }
         }
         else{
             if(isItAMine == true){
                 return "[⎈]";
             }
-            else if(getMinesNear == 0){
-                return "   ";
+            else if(getMinesNear() == 0){
+                return "[B]";
             }
             else{
-                return "(" + minesNear + ")"
+                return "(" + minesNear + ")";
                 }
            
         }
-        
+}
             
-           
-
-
-   
 }
