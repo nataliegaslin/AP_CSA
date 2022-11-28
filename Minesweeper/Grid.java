2ca135ccb13c75f44ae140bd057a2e56022379d0
nataliegaslin <112636317+nataliegaslin@users.noreply.github.com>
2022-11-28 09:14:29 -0500

    Update Grid.java

--- Minesweeper/Grid.java
+++ Minesweeper/Grid.java
@@ -1,33 +1,33 @@
 public class Grid {  
-    private int xSize;
-    private int ySize;
-    private int mineNum;
+    private int rows;
+    private int cols;
+    private int mines;
     private Tile[][] myGrid;
    
 
 
-    public Grid(int xDim, int yDim, int num){ //setting grid x and y dimensions
-        ySize = yDim;
-        xSize = xDim;
-        mineNum = num;
-        myGrid = new Tile[xSize][ySize];
+    public Grid(int rowNum, int colNum, int mineNum){ //setting grid x and y dimensions
+        rows = rowNum;
+        cols = colNum;
+        mines = mineNum;
+        myGrid = new Tile[rows][cols];
+	    makeGrid();
 
     }
 
-    public void makeGrid(){
-
-        for(int i = 0; i< mineNum; i++){
-            int x = (int) (Math.random() * xSize);
-            int y = (int) (Math.random() * ySize);
+    public void makeGrid(){ //makes a grid with the correct number of rows, cols, and mines
+        for(int i = 0; i< mines; i++){
+            int row = (int) (Math.random() * rows);
+            int col = (int) (Math.random() * cols);
             
-            while (myGrid[x][y] != null){
-                int x = (int) (Math.random() * xSize);
-                int y = (int) (Math.random() * ySize);
+            while (myGrid[row][col] != null){
+                row = (int) (Math.random() * rows);
+                col = (int) (Math.random() * cols);
             }
         
             Tile myTile = new Tile();
-            myGrid[x][y] = myTile;
-            myGrid[x][y].markMine();
+            myGrid[row][col] = myTile;
+            myGrid[row][col].markMine();
             
         }
         
@@ -36,42 +36,50 @@ public class Grid {
                 if(myGrid[i][j] == null){
                     Tile blankTile = new Tile();
                     myGrid[i][j] = blankTile;
-                    blankTile.neighborMines(mineNum());
+                    blankTile.setMinesNear(minesNear(i, j));                
                 }
             }
         }
       
     }
     
-    public int mineNum(){
+    public int minesNear(int i, int j){ //determines how many mines are touching a safe tile
         int numMines = 0;
-         for(int i =0; i<myGrid.length; i++){
-            for(int j=0; j<myGrid[i].length; j++){
-                numMines = 0;
-                if(myGrid[i][j].isMine() == false){
-                    for(int m = -1; m<2; m++){
-                        for(int n = -1; n<2; n++){
-                            if(i+m > - 1 && j+n > -1 && i+m < myGrid.length && j+n < myGrid[i].length){
-
-                                if(myGrid[i+m][j+n].isMine() == true){
-                                    numMines++;
-                                }
-                            }
+	    if(myGrid[i][j].isMine() == false){
+            for(int m = -1; m<2; m++){
+                for(int n = -1; n<2; n++){
+                    if(i+m > - 1 && j+n > -1 && i+m < myGrid.length && j+n < myGrid[i].length){
+			            if(myGrid[i+m][j+n] != null && myGrid[i+m][j+n].isMine() == true){
+                           numMines++;
                         }
                     }
                 }
-               return numMines;
-        
-                }
-
-            } 
+            }
+        } 
+        return numMines;
         }
-     
+
+    public Tile returnTile(int rowCoor, int colCoor){ //to access a specific tile in myGrid
+        return myGrid[rowCoor][colCoor];
     }
-     public static void main(String[] args){
-        Grid newGrid = new Grid(6, 5, 8);
-        newGrid.makeGrid();
-        System.out.println(mineNum());
+
+    public String toString(){ //adds each tile in a given row to a string and then creates a new line for a new row
+        String printGrid = "";
+        for(int i = 0; i<myGrid.length; i++){
+            for(int j = 0; j<myGrid[i].length; j++){  
+                printGrid +=myGrid[i][j];
+            }
+            printGrid += "\n";
+        }
+        return printGrid;
     }
+    public void revealAll(){ //flips all of the tiles (to be used at the end of the game)
+        for(int i = 0; i<myGrid.length; i++){
+            for(int j = 0; j<myGrid[i].length; j++){
+                myGrid[i][j].flip();
+            }
+        }
+    }
+
    
 }
