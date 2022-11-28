29adc86526d5d304ffdddf60ddb855a503874410
nataliegaslin <112636317+nataliegaslin@users.noreply.github.com>
2022-11-28 09:17:09 -0500

    Update Main.java

--- Minesweeper/Main.java
+++ Minesweeper/Main.java
@@ -1,7 +1,37 @@
+import java.util.*;
 public class Main{
-    // Game minesweeper = new Game(int, int);
-    // minesweeper.playRound();
-    public static void main(String[] args){
-        makeGrid();
+
+    public static void main(String[] args){ //gets inputs from the user and actually plays the game: determines whether the player wants to flip or flag a tile
+        Scanner sc = new Scanner(System.in);
+        System.out.print("How many rows in your grid?");
+        int rowNum = sc.nextInt();
+        System.out.print("How many columns in your grid?");
+        int colNum = sc.nextInt();
+        System.out.print("How many mines in your grid?");
+        int mineNum = sc.nextInt();
+        sc.nextLine();
+
+        Game newGame = new Game(rowNum, colNum, mineNum);
+        newGame.printOut();
+        while(newGame.isGameOver() == false){
+            String input = sc.nextLine();
+            if(input.substring(0,1).equals("f")){
+                int indexNum = input.indexOf(" ");
+                int indexNumTwo = input.indexOf(" ", indexNum+1);
+                int rowCoor = Integer.parseInt(input.substring(indexNum+1, indexNumTwo));
+                int colCoor = Integer.parseInt(input.substring(indexNumTwo+1, input.length()));
+                newGame.flag(rowCoor, colCoor);
+                newGame.printOut();
+            }
+            else{
+                int indexNum = input.indexOf(" ");
+                int rowCoor = Integer.parseInt(input.substring(0, indexNum));
+                int colCoor = Integer.parseInt(input.substring(indexNum+1, input.length()));
+                newGame.flip(rowCoor, colCoor);
+                newGame.printOut();
+            }
+        }
+        newGame.endMessage();
+        
     }
 }
