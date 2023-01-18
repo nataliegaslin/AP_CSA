e429e9a4d6727b42590f51215df79a12ed0b150a
nataliegaslin <112636317+nataliegaslin@users.noreply.github.com>
2023-01-17 19:12:48 -0500

    Update Fictionbook.java

--- Library/Fictionbook.java
+++ Library/Fictionbook.java
@@ -1,3 +1,10 @@
 public class FictionBook extends Book{
-    
+    public FictionBook(String line){
+        super(line);
+    }
+    public void printInfo(){
+        System.out.println("title:" + title);
+        System.out.println("author:" + author);
+        System.out.println("edition: " + edition);
+    }
 }
