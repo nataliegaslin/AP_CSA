414a58302136b43e0cbfc6dbb6ea66df8b01af44
nataliegaslin <112636317+nataliegaslin@users.noreply.github.com>
2023-01-17 19:12:23 -0500

    Update Book.java

--- Library/Book.java
+++ Library/Book.java
@@ -1,20 +1,60 @@
+import java.lang.*;
 import java.io.*;
-import java.util.Scanner;
+
 
 abstract class Book{
     String title;
     String genre;
     String subject;
+    String author;
     int edition;
+    int quantity;
     
-    public Book(){
-
+    public Book(String line){
+        String [] bookStuff = line.split(",");
+        title = bookStuff[0];
+        genre = bookStuff[1];
+        author = bookStuff[2];
+        subject = bookStuff[3];
+        edition = Integer.parseInt(bookStuff[4]);
+        quantity = 1;
     }
     public void printInfo(){
-        System.out.println(title + genre + subject + edition);
+        System.out.println("title:" + title);
+        System.out.println("genre:" + genre);
+        System.out.println("author:" + author);
     }
-
+    public void incQuantity(){
+        quantity++;
+    }
+    public void decQuantity(){
+        quantity--;
+    }
+ public int getQuantity(){
+        return quantity;
+    }
     public String getTitle(){
         return title;
+    }
+    public int getEdition(){
+        return edition;
+    }
+    public String getSubject(){
+        return subject;
+    }
+    public String getAuthor(){
+        return author;
+    }
+    public String getGenre(){
+        return genre;
+    }
+
+    public boolean equals(Book otherBook){
+        if(otherBook.title.equals(this.title) && otherBook.author.equals(author)&& otherBook.subject.equals(subject)&& otherBook.genre.equals(genre)&& otherBook.edition == edition){
+                return true;
+        }
+        else{
+            return false;
+        }
     }
 }
