1500578b67a37d0ded574fe090b4cfc8de827797
nataliegaslin <112636317+nataliegaslin@users.noreply.github.com>
2023-01-17 19:13:32 -0500

    Update Textbook.java

--- Library/Textbook.java
+++ Library/Textbook.java
@@ -1,10 +1,12 @@
 public class Textbook extends Book{
-    public Textbook(){
-        Scanner sc = new Scanner(BookList.csv);
-        String mystring = sc.nextLine;
-        String [] words = mystring.split(“,”);
-        if(words[1].equals("Textbook")){
-            super.bookList.add()
-        }
+    public Textbook(String line){
+        super(line);
+    }
+
+    public void printInfo(){
+        super.printInfo();
+        System.out.println("subject: " + subject);
+        System.out.println("edition: " + edition);
+        
     }
 }
