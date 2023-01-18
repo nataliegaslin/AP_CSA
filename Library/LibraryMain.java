073a93fc2303b802b080fa2a85ec9dacb842c69a
nataliegaslin <112636317+nataliegaslin@users.noreply.github.com>
2023-01-17 19:14:56 -0500

    Add files via upload

---
+++ Library/LibraryMain.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+import java.io.File;
+public class LibraryMain{
+
+    public static void main(String[] args){
+        BookList myList = new BookList();
+        String bookListString = "";
+        try{
+            String listOfBooks = "BookList.csv";
+            File myFile = new File(listOfBooks);
+            Scanner sc = new Scanner(myFile);
+        while(sc.hasNextLine()){
+            bookListString = sc.nextLine();
+            String splitString[] = bookListString.split(",");
+            if (splitString[1].equals("Fiction")){
+                Book myBook = new FictionBook(bookListString);
+                myList.addToList(myBook);
+            }
+            if (splitString[1].equals("Nonfiction")){
+                Book myBook = new NonfictionBook(bookListString);
+                myList.addToList(myBook);
+            }
+            if (splitString[1].equals("Textbook")){
+                Book myBook = new Textbook(bookListString);
+                myList.addToList(myBook);
+            }
+        }
+        myList.sortList("Genre");
+        myList.printTable();
+        }
+        catch(Exception e){
+            System.out.println("That's a fail");
+        }
+
+    }
+}
